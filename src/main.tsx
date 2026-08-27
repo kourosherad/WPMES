49f04d66b0e7936fd9d69436df351e22f0be4463
Wagon Pars MES <mes@wagonpars.local>
2026-08-27 07:40:13 +0330

    Redesign WPMES with light industrial signature theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppV3 from './AppV3';
 import './flagship.css';
+import './light-industrial.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
